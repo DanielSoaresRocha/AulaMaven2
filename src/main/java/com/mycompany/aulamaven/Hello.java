7604d61d64eccb21c7d65c0e1a9a06208ace3957
Daniel Soares <legoliga123@gmail.com>
2019-04-17 14:29:06 -0300

    inicio

---
+++ src/main/java/com/mycompany/aulamaven/Hello.java
@@ -0,0 +1,12 @@
+package com.mycompany.aulamaven;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class Hello {
+    
+    public String dizHello(){
+        return "Hello from ManagedBean";
+    }
+    
+}
